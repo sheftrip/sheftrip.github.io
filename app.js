172ffd03cba38434c3e8851dc75cc066b3c464dc
Shefali Tripathi <47482239+sheftrip@users.noreply.github.com>
2020-06-01 23:27:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-////
+// Selectors
+
+const abt = document.querySelector(".about");
+
+//Event Listeners
+
+abt.addEventListener('click', goToLink);
+
+//Functions
+
+function goToLink(event){
+    const item = event.target;
+    if(item.classList[0] === 'linkedin-btn'){
+        window.location.href = "https://in.linkedin.com/in/shefali-tripathi-529474167";
+    }
+
+    if(item.classList[0] === 'git-btn'){
+        window.location.href = "https://github.com/sheftrip";
+    }
+    
+}
